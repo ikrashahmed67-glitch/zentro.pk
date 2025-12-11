3e3fddaf5a1fbc8c5360ea21f3c18bdc7ba69a5c
ikrashahmed67-glitch <ikrashahmed67@gmail.com>
2025-12-11 22:48:16 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,6 +11,12 @@ const nextConfig = {
       },
     ],
   },
+
+  // MOST IMPORTANT PARTS ↓↓↓
+  experimental: {
+    appDir: true,     // tells Next.js this is an App Router project
+  },
+  output: "standalone",  // required by Vercel to generate manifest
 };
 
 module.exports = nextConfig;
